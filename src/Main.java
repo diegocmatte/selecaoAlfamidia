229f043a94b6c9e222e6eb5d0c94978f9d37379b
Diego Matté <diegocmatte@gmail.com>
2022-01-08 01:15:59 -0300

    ajustando validacoes das datas

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.time.YearMonth;
 import java.util.*;
 
 public class Main {
 
-    private static ArrayList<Pessoa> lista = new ArrayList<>();
-    private static final Integer ANO_ATUAL = 2022;
+    private static final ArrayList<Pessoa> lista = new ArrayList<>();
+    private static final Integer ANO_ATUAL = YearMonth.now().getYear();
 
     public static void menu() {
         System.out.println("|-------------------------------|");
@@ -255,28 +256,25 @@ public class Main {
             return false;
         }
 
-        if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
-            if (dia > 31) {
-                System.out.println("Data inválida para mês de 31 dias.");
-                return false;
-            }
-        } else {
-            if (mes == 2) {
-                if (((ano % 4 == 0) && (ano % 100 != 0)) || (ano % 400 == 0)) {
-                    if (dia > 29) {
-                        System.out.println("Dia inválido para Fevereiro (ano bissexto).");
-                        return false;
-                    }
-                } else if (dia > 28) {
-                    System.out.println("Dia inválido para Fevereiro (ano não bissexto).");
-                    return false;
-                }
-            }
+        if (mes == 4 || mes == 6 || mes == 5 || mes == 9 || mes == 11) {
             if (dia > 30) {
                 System.out.println("Data inválida para mês de 30 dias.");
                 return false;
             }
         }
+
+        if (mes == 2) {
+            if (((ano % 4 == 0) && (ano % 100 != 0)) || (ano % 400 == 0)) {
+                if (dia > 29) {
+                    System.out.println("Dia inválido para Fevereiro (ano bissexto).");
+                    return false;
+                }
+            } else if (dia > 28) {
+                System.out.println("Dia inválido para Fevereiro (ano não bissexto).");
+                return false;
+            }
+        }
+
 
         return true;
     }
